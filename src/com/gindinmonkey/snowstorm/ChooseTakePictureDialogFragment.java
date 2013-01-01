0a5952f30f970d5f042f5484ab09e468792263ba
Lapprenti <bad_shika_87@hotmail.fr>
2013-01-01 23:52:07 +0100

    Adding Dialog Box for choosing Picture source

---
+++ src/com/gindinmonkey/snowstorm/ChooseTakePictureDialogFragment.java
@@ -0,0 +1,28 @@
+package com.gindinmonkey.snowstorm;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class ChooseTakePictureDialogFragment extends DialogFragment {
+	
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    	
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder	.setTitle(R.string.pick_picture)
+		        .setItems(R.array.pick_picture_array, new DialogInterface.OnClickListener() {
+		            public void onClick(DialogInterface dialog, int which) {
+		            // The 'which' argument contains the index position
+		            // of the selected item
+		            }
+		        });
+        // Create the AlertDialog object and return it
+        return builder.create();
+        
+    }
+    
+}
--- src/com/gindinmonkey/snowstorm/HomeActivity.java
+++ src/com/gindinmonkey/snowstorm/HomeActivity.java
@@ -2,21 +2,38 @@ package com.gindinmonkey.snowstorm;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.view.Menu;
+import android.view.View;
 
 public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+    	
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+        
+    }
+    
+    public void startTakePicture(View view) {
+
+    	DialogFragment dialog = new ChooseTakePictureDialogFragment();
+        dialog.show(getFragmentManager(), "ChooseTakePictureDialogFragment");
+    	
+    }
+    
+    public void startGallery(View view) {
+        // Do something in response to button click
     }
     
 }
